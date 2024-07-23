bb2af1699c496d7244865faeb943d6004ab25e99
IoannaMaritsa <sdi2000237@di.uoa.gr>
2024-07-23 20:39:57 +0300

    home page 3

--- react-app/src/components/MainBottom.js
+++ react-app/src/components/MainBottom.js
@@ -5,9 +5,9 @@ const MainBottom = () => {
   return (
     <div className="main-bottom">
       <div className="column">
-        <img src="/path-to-your-icon1.png" alt="icon1" className="icon" />
-        <img src="/path-to-your-icon2.png" alt="icon2" className="icon" />
-        <img src="/path-to-your-icon3.png" alt="icon3" className="icon" />
+        <img src="/twitter.png" alt="icon1" className="icon" />
+        <img src="/instagram.png" alt="icon2" className="icon" />
+        <img src="/youtube.png" alt="icon3" className="icon" />
       </div>
       <div className="column">
         <p>Your text for column 2 here</p>
