dd1db2386e334eaff55b3639df42edbd0e6772ad
IoannaMaritsa <sdi2000237@di.uoa.gr>
2024-07-23 21:29:41 +0300

    ηομε 4

--- react-app/src/components/MainBottom.js
+++ react-app/src/components/MainBottom.js
@@ -5,18 +5,29 @@ const MainBottom = () => {
   return (
     <div className="main-bottom">
       <div className="column">
-        <img src="/twitter.png" alt="icon1" className="icon" />
-        <img src="/instagram.png" alt="icon2" className="icon" />
-        <img src="/youtube.png" alt="icon3" className="icon" />
+        <div className="image-row">
+          <img src="/twitter.png" alt="icon1" className="icon" />
+          <img src="/instagram.png" alt="icon2" className="icon" />
+          <img src="/youtube.png" alt="icon3" className="icon" />
+        </div>
       </div>
       <div className="column">
-        <p>Your text for column 2 here</p>
+        <b>Γενικά</b>
+        <p>Εγγραφή</p>
+        <p>Κέντρο Βοήθειας</p>
+        <p>FAQ</p>
       </div>
       <div className="column">
-        <p>Your text for column 3 here</p>
+        <b>Εταιρεία</b>
+        <p>About Us</p>
+        <p>Γιατί το CareerHive;</p>
+        <p>Επικοινωνία</p>
       </div>
       <div className="column">
-        <p>Your text for column 4 here</p>
+        <b>Πληροφορίες</b>
+        <p>Όροι και προυποθέσεις</p>
+        <p>Πολιτική Απορρήτου</p>
+        <p>Πολιτική Cookies</p>
       </div>
     </div>
   );
