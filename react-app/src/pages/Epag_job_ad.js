292bc72bb847d13f8f54e8278b1735fd58f38d49
IoannaMaritsa <sdi2000237@di.uoa.gr>
2024-07-28 00:44:12 +0300

    professional-home-page

---
+++ react-app/src/pages/Epag_job_ad.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import MainBottom from '../components/MainBottom';
+import '../css/home-page.css';
+
+export default function Epag_job_ad() {
+  return(
+    <div>
+      <Header variant="professional" />
+      
+      <MainBottom />
+      <Footer />
+    </div>
+  )
+}
