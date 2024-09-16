df3f53cc3fc39153dc2b697bb1912991a8d59008
IoannaMaritsa <sdi2000237@di.uoa.gr>
2024-09-16 16:00:43 +0300

    help final

--- react-app/src/pages/About.js
+++ react-app/src/pages/About.js
@@ -2,46 +2,68 @@ import React from "react"
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import MainBottom from '../components/MainBottom';
+import useScrollToTop from '../hooks/useScrollToTop';
+import { useNavigate } from 'react-router-dom';
 import '../css/help.css';
 
 export default function About() {
-  return(
+  useScrollToTop();
+
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1); // Navigate to the previous page
+  };
+
+  return (
     <div>
       <Header variant="homepage" />
-      <div className="split-page">
-        <div className="text-section">
-          <h1>Πληροφορίες για το CareerHive</h1>
-          <p>Το CareerHive είναι ο κορυφαίος ιστότοπος θέσεων εργασίας στον κόσμο με περισσότερους από 800.000 μοναδικούς επισκέπτες. 
-            Το CareerHive δίνει προτεραιότητα στα άτομα που αναζητούν εργασία, παρέχοντάς τους δωρεάν πρόσβαση για την αναζήτηση θέσεων εργασίας, τη δημοσίευση βιογραφικών και τη διερεύνηση εταιρειών. 
-            Καθημερινά συνδέουμε εκατομμύρια άτομα με νέες ευκαιρίες.
-          </p>
+      <div className="help-secondary-section">
+        <div className="help-back-icon-container">
+          <img
+            src="/back-icon.png" // Replace with your icon path
+            alt="Back"
+            className="back-icon"
+            onClick={handleBackClick}
+          />
         </div>
-        <div className="image-section">
-          <img src="/about.png" alt="Right Section" className="image" />
-        </div>
-      </div>
+        <div className="help-main-container">
+          <div className="split-page">
+            <div className="about-text-section">
+              <h1>Πληροφορίες για το CareerHive</h1>
+              <p>Το CareerHive είναι ο κορυφαίος ιστότοπος θέσεων εργασίας στον κόσμο με περισσότερους από 800.000 μοναδικούς επισκέπτες.
+                Το CareerHive δίνει προτεραιότητα στα άτομα που αναζητούν εργασία, παρέχοντάς τους δωρεάν πρόσβαση για την αναζήτηση θέσεων εργασίας, τη δημοσίευση βιογραφικών και τη διερεύνηση εταιρειών.
+                Καθημερινά συνδέουμε εκατομμύρια άτομα με νέες ευκαιρίες.
+              </p>
+            </div>
+            <div className="about-image-section">
+              <img src="/about.png" alt="Right Section" className="image" />
+            </div>
+          </div>
 
-      <div className="split-page">
-        <div className="image-section">
-          <img src="/businessleader.jpg" alt="Left Section" className="image" />
-        </div>
-        <div className="text-section">
-          <h1>Η ηγεσία μας</h1>
-          <p>
-            Η ηγετική ομάδα της CareerHive είναι πολυποίκιλη, αφοσιωμένη και δεσμευμένη στην ενίσχυση των εργαζομένων μας για την εκπλήρωση της αποστολής μας: Να βοηθάμε τους ανθρώπους να βρίσκουν θέσεις εργασίας. Καλλιεργώντας ισχυρές σχέσεις και συνεργασία, εξυπηρετούν και υποστηρίζουν τα άτομα που αναζητούν εργασία, τους εργοδότες, την κοινωνία και τους εργαζομένους μας.
-          </p>
-        </div>
-      </div>
+          <div className="split-page">
+            <div className="about-image-section">
+              <img src="/businessleader.jpg" alt="Left Section" className="image" />
+            </div>
+            <div className="about-text-section">
+              <h1>Η ηγεσία μας</h1>
+              <p>
+                Η ηγετική ομάδα της CareerHive είναι πολυποίκιλη, αφοσιωμένη και δεσμευμένη στην ενίσχυση των εργαζομένων μας για την εκπλήρωση της αποστολής μας: Να βοηθάμε τους ανθρώπους να βρίσκουν θέσεις εργασίας. Καλλιεργώντας ισχυρές σχέσεις και συνεργασία, εξυπηρετούν και υποστηρίζουν τα άτομα που αναζητούν εργασία, τους εργοδότες, την κοινωνία και τους εργαζομένους μας.
+              </p>
+            </div>
+          </div>
 
-      <div className="split-page">
-        <div className="text-section">
-          <h1>Οι δεσμεύσεις μας</h1>
-          <p>
-          Οι δεσμεύσεις μας περί περιβάλλοντος, κοινωνικών ζητημάτων και διακυβέρνησης (Environmental, Social & Governance – ESG) έχουν σκοπό να οδηγήσουν σε ένα εργασιακό μέλλον που θα είναι δίκαιο και χωρίς διακρίσεις. Ως ο κορυφαίος ιστότοπος θέσεων εργασίας στον κόσμο και η πρωταγωνιστική πλατφόρμα αντιστοιχίσεων και προσλήψεων, προσπαθούμε να δημιουργήσουμε έναν θετικό αντίκτυπο στην κοινωνία, συνδέοντας άτομα με καλύτερες δουλειές για τη βελτίωση της ζωής τους.
-          </p>
-        </div>
-        <div className="image-section">
-          <img src="/activist.jpg" alt="Right Section" className="image" />
+          <div className="split-page">
+            <div className="about-text-section">
+              <h1>Οι δεσμεύσεις μας</h1>
+              <p>
+                Οι δεσμεύσεις μας περί περιβάλλοντος, κοινωνικών ζητημάτων και διακυβέρνησης (Environmental, Social & Governance – ESG) έχουν σκοπό να οδηγήσουν σε ένα εργασιακό μέλλον που θα είναι δίκαιο και χωρίς διακρίσεις. Ως ο κορυφαίος ιστότοπος θέσεων εργασίας στον κόσμο και η πρωταγωνιστική πλατφόρμα αντιστοιχίσεων και προσλήψεων, προσπαθούμε να δημιουργήσουμε έναν θετικό αντίκτυπο στην κοινωνία, συνδέοντας άτομα με καλύτερες δουλειές για τη βελτίωση της ζωής τους.
+              </p>
+            </div>
+            <div className="about-image-section">
+              <img src="/activist.jpg" alt="Right Section" className="image" />
+            </div>
+          </div>
         </div>
       </div>
 
--- react-app/src/pages/CooPolicy.js
+++ react-app/src/pages/CooPolicy.js
@@ -3,21 +3,36 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import MainBottom from '../components/MainBottom';
 import useScrollToTop from '../hooks/useScrollToTop';
+import { useNavigate } from 'react-router-dom';
 import '../css/help.css';
 
 
 export default function CooPolicy() {
   useScrollToTop();
-  return(
+
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1); // Navigate to the previous page
+  };
+
+  return (
     <div>
       <Header variant="homepage" />
       <div className="help-main-section">
-      
-      <div className="help-main-container">
-        <h2>Θέλουμε το CareerHive να αντικατοπτρίζει την καλύτερη εκδοχή της επαγγελματικής ζωής</h2>
-        <p>Αυτή είναι μια κοινότητα όπου αντιμετωπίζουμε ο ένας τον άλλον με σεβασμό και βοηθάμε ο ένας τον άλλον να πετύχει.</p>
-        <br></br>
-        <div className="community-frame">
+        <div className="help-back-icon-container">
+          <img
+            src="/back-icon.png" // Replace with your icon path
+            alt="Back"
+            className="back-icon"
+            onClick={handleBackClick}
+          />
+        </div>
+        <div className="help-main-container">
+          <h2>Θέλουμε το CareerHive να αντικατοπτρίζει την καλύτερη εκδοχή της επαγγελματικής ζωής</h2>
+          <p>Αυτή είναι μια κοινότητα όπου αντιμετωπίζουμε ο ένας τον άλλον με σεβασμό και βοηθάμε ο ένας τον άλλον να πετύχει.</p>
+          <br></br>
+          <div className="community-frame">
             <div className="community-box">
               <h5>Μείνετε ασφαλείς</h5>
               <h6>Ξεκινήστε μόνο ασφαλείς συνομιλίες στο CareerHive.</h6>
@@ -30,24 +45,24 @@ export default function CooPolicy() {
               <h5>Να είστε επαγγελματίες</h5>
               <h6>Επιτρέπουμε ευρείες συζητήσεις για τον κόσμο της εργασίας, αλλά απλώς κρατήστε τον επαγγελματικό.</h6>
             </div>
-        </div>
-        <h2>Επαγγελματικές κοινοτικές πολιτικές του CareerHive</h2>
-        <p>Σας ευχαριστούμε που χρησιμοποιείτε το CareerHive, όπου οι επαγγελματίες του κόσμου συγκεντρώνονται για να βρουν θέσεις εργασίας, να παραμείνουν ενημερωμένοι, να μάθουν νέες δεξιότητες και να δημιουργήσουν παραγωγικές σχέσεις. Το περιεχόμενο που συνεισφέρετε θα πρέπει να προστεθεί στην κοινότητα του CareerHive με εποικοδομητικό τρόπο.</p>
-        <ol>
+          </div>
+          <h2>Επαγγελματικές κοινοτικές πολιτικές του CareerHive</h2>
+          <p>Σας ευχαριστούμε που χρησιμοποιείτε το CareerHive, όπου οι επαγγελματίες του κόσμου συγκεντρώνονται για να βρουν θέσεις εργασίας, να παραμείνουν ενημερωμένοι, να μάθουν νέες δεξιότητες και να δημιουργήσουν παραγωγικές σχέσεις. Το περιεχόμενο που συνεισφέρετε θα πρέπει να προστεθεί στην κοινότητα του CareerHive με εποικοδομητικό τρόπο.</p>
+          <ol>
             <li>Πείτε μας εάν βλέπετε καταχρηστικό περιεχόμενο</li>
             <li>Μην δημοσιεύετε παρενοχλητικό περιεχόμενο</li>
             <li>Μην απειλείτε, υποκινείτε ή προωθείτε τη βία</li>
             <li>Μην μοιράζεστε υλικό που απεικονίζει την εκμετάλλευση παιδιών</li>
             <li>Μην προωθείτε, πουλάτε ή επιχειρείτε να αγοράσετε παράνομα ή επικίνδυνα αγαθά ή υπηρεσίες.</li>
             <li>Μην κοινοποιείτε περιεχόμενο που προωθεί επικίνδυνους οργανισμούς ή άτομα.</li>
-        </ol>
-        <div className="split-page">
-          <img src="/danger.png" alt="icon1" className="icon" />
-            <p style={{marginLeft:'20px', fontStyle:'italic'}}>Η παραβίαση των πολιτικών της κοινότητάς μας μπορεί να οδηγήσει σε ενέργειες κατά του λογαριασμού ή του περιεχομένου σας</p>
+          </ol>
+          <div className="split-page">
+            <img src="/danger.png" alt="icon1" className="icon" />
+            <p style={{ marginLeft: '20px', fontStyle: 'italic' }}>Η παραβίαση των πολιτικών της κοινότητάς μας μπορεί να οδηγήσει σε ενέργειες κατά του λογαριασμού ή του περιεχομένου σας</p>
+          </div>
+
         </div>
-        
       </div>
-    </div>
       <MainBottom />
       <Footer />
     </div>
